1419d19e92b65e48d28c78db992c5aaced93027f
Maria Korchagina <kobysheva_maria@mail.ru>
2022-12-20 23:33:47 +0300

    ui for redux

--- src/index.js
+++ src/index.js
@@ -6,22 +6,46 @@ const reducer = (state = 0, action) => {
     case 'INC':
       return state + 1;
 
+    case 'DEC':
+      return state - 1;
+
     default:
       return state;
   }
 };
 
 const store = createStore(reducer);
-store.subscribe(() => {
+
+document
+  .getElementById('inc')
+  .addEventListener('click', () => {
+    store.dispatch({ type: 'INC' });
+  });
+
+document
+  .getElementById('dec')
+  .addEventListener('click', () => {
+    store.dispatch({ type: 'DEC' });
+  });
+
+const update = () => {
+  document
+    .getElementById('counter')
+    .innerHTML = store.getState();
+}
+
+store.subscribe(update)
+
+//store.subscribe(() => {
   //store.subscribe(() - подписаться на обновление store
-  console.log(store.getState());
+  //console.log(store.getState());
   //store.getState()) - получаем текущий state;
-})
+//})
 
 // console.log(store.getState());
-store.dispatch({ type: 'INC' });
+//store.dispatch({ type: 'INC' });
 //store.dispatch({ type: 'INC' }); - для того,чтобы обработать какое-то действие
-store.dispatch({ type: 'INC' });
+//store.dispatch({ type: 'INC' });
 // console.log(store.getState());
 
   //   if (action.type === 'INC') {
