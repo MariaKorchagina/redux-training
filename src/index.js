24e0226740e837216811a9b028d32158521c598a
Maria Korchagina <kobysheva_maria@mail.ru>
2022-12-20 23:48:06 +0300

    added files actions and reducer in project

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
 import { createStore } from 'redux';
+import reducer from './reducer';
+import {inc, dec, rnd} from './actions';
 
-const reducer = (state = 0, action) => {
-  switch (action.type) {
+// const reducer = (state = 0, action) => {
+//   switch (action.type) {
 
-    case 'INC':
-      return state + 1;
+//     case 'INC':
+//       return state + 1;
 
-    case 'DEC':
-      return state - 1;
+//     case 'RND':
+//       return state + action.payload;
 
-    default:
-      return state;
-  }
-};
+//     case 'DEC':
+//       return state - 1;
+
+//     default:
+//       return state;
+//   }
+// };
 
 const store = createStore(reducer);
+
+// const inc = () => ({ type: 'INC' });
+// const dec = () => ({ type: 'DEC' });
+// const rnd = (payload) => ({ type: 'RND', payload });
+
 
 document
   .getElementById('inc')
   .addEventListener('click', () => {
-    store.dispatch({ type: 'INC' });
+    store.dispatch(inc());
   });
 
 document
   .getElementById('dec')
   .addEventListener('click', () => {
-    store.dispatch({ type: 'DEC' });
+    store.dispatch(dec());
+  });
+
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+    store.dispatch(rnd(payload));
   });
 
 const update = () => {
